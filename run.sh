ae93d26c9d8b787b9b63361aa9ed2961fac96e45
t22 <t22@t22.com>
2021-02-25 07:45:48 +0800

    add sh

---
+++ run.sh
@@ -0,0 +1,70 @@
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt -e 20MB/test1-Split.csv http://t22.tfcis.org:10022/file/upload > 20MB/test1-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt -e 20MB/test1-Origin.csv http://t22.tfcis.org:10022/file/upload2 > 20MB/test1-Origin.txt
+sleep 1m
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test2-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test2-Origin.txt
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test3-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test3-Origin.txt
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test4-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test4-Origin.txt
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test5-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test5-Origin.txt
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test6-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test6-Origin.txt
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test7-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test7-Origin.txt
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test8-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test8-Origin.txt
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test9-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test9-Origin.txt
+
+dd if=/dev/urandom of=data/20MB.txt bs=20MB count=1
+cp blank.txt 20MB.txt
+base64 data/20MB.txt >> 20MB.txt
+echo "--1234567890--" >> 20MB.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload > 20MB/test10-Split.txt
+ab -T "multipart/form-data; boundary=1234567890" -n 30 -c 1 -p 20MB.txt http://t22.tfcis.org:10022/file/upload2 > 20MB/test10-Origin.txt
